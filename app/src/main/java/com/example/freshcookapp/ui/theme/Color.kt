4368a3b552787938acd4fe188db06d82817c8507
vocaotangoc <caongoctanvo@gmail.com>
2025-10-22 00:53:21 +0700

    thêm cấu trúc cho app font và màu chủ đạo
    Signed-off-by: vocaotangoc <caongoctanvo@gmail.com>

---
+++ app/src/main/java/com/example/freshcookapp/ui/theme/Color.kt
@@ -0,0 +1,24 @@
+package com.example.freshcookapp.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+val Purple80 = Color(0xFFD0BCFF)
+val PurpleGrey80 = Color(0xFFCCC2DC)
+val Pink80 = Color(0xFFEFB8C8)
+
+val Purple40 = Color(0xFF6650a4)
+val PurpleGrey40 = Color(0xFF625b71)
+val Pink40 = Color(0xFF7D5260)
+
+val Cinnabar50 = Color(0xFFFEF3F2)
+val Cinnabar100 = Color(0xFFFEE3E2)
+val Cinnabar200 = Color(0xFFFDCDCB)
+val Cinnabar300 = Color(0xFFFBAAA6)
+val Cinnabar400 = Color(0xFFF77872)
+val Cinnabar500 = Color(0xFFEB3830) // 🔥 màu chính (primary)
+val Cinnabar600 = Color(0xFFDA3028)
+val Cinnabar700 = Color(0xFFB7251E)
+val Cinnabar800 = Color(0xFF98211C)
+val Cinnabar900 = Color(0xFF7E221E)
+val Cinnabar950 = Color(0xFF440D0B)
+
---
+++ app/src/main/java/com/example/freshcookapp/ui/theme/Font.kt
@@ -0,0 +1,13 @@
+package com.example.freshcookapp.ui.theme
+
+import androidx.compose.ui.text.font.Font
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontWeight
+import com.example.freshcookapp.R
+
+val WorkSans = FontFamily(
+    Font(R.font.worksans_regular, FontWeight.Normal),
+    Font(R.font.worksans_medium, FontWeight.Medium),
+    Font(R.font.worksans_semibold, FontWeight.SemiBold),
+    Font(R.font.worksans_bold, FontWeight.Bold)
+)
